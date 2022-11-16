deae7790748888ba5b4985856146b509a91e3fd5
jinlucao <jinlc@schbrain.com>
2022-11-16 11:26:34 +0800

    fix: test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
-    <rs-button type="cancel">按钮</rs-button>
+    <rs-button type="cancel" @click="test">按钮</rs-button>
   </div>
 </template>
 
 <style lang="less">
 
 </style>
+
+<script lang="ts">
+import { Vue, Component} from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  private test() {
+    console.log('121212')
+  }
+}
+</script>
